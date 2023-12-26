ecfce84e2391dd64afe1deb385c4b17ad58f6768
1Chinmay1 <phchinmay80@gmail.com>
2023-12-26 12:59:55 +0530

    updated function get data Modules

--- golden_crossover.py
+++ golden_crossover.py
@@ -1,29 +1,12 @@
 import pandas as pd
 import yfinance as yf
-
-# Function to fetch stock symbols
-def fetchCodes():
-    url = "https://archives.nseindia.com/content/equities/EQUITY_L.csv"
-    return list(pd.read_csv(url)['SYMBOL'].values)
-
-# Function to fetch stock data
-def fetchStockData(stockCode, period, duration):
-    try:
-        append_exchange = ".NS"
-        data = yf.download(
-            tickers=stockCode + append_exchange,
-            period=period,
-            interval=duration)
-        return data
-    except Exception as e:
-        print(f"Error fetching data for {stockCode}: {e}")
-        return None
+import fetcher
 
 # Function to apply Golden Crossover strategy
 def golden_crossover_strategy(stock_symbols, period='1y', duration='1d'):
     selected_stocks = []
     for symbol in stock_symbols:
-        stock_data = fetchStockData(symbol, period, duration)
+        stock_data = fetcher.get_stock_data(symbol, period, duration)#function to fetch stock data
         if stock_data is not None and len(stock_data) >= 26:  # Ensure sufficient data for EMA calculation
             stock_data['EMA5'] = stock_data['Close'].ewm(span=5, min_periods=0, adjust=False).mean()
             stock_data['EMA13'] = stock_data['Close'].ewm(span=13, min_periods=0, adjust=False).mean()
@@ -39,7 +22,7 @@ def golden_crossover_strategy(stock_symbols, period='1y', duration='1d'):
                     df_selected_stocks.to_csv('golden_crossover_selected_stocks.csv', index=False)
 
 # Fetch stock symbols
-stock_symbols = fetchCodes()
+stock_symbols = fetcher.get_stock_codes()#function to fetch Stock Codes
 
 # Apply Golden Crossover strategy and store selected stocks in CSV
 golden_crossover_strategy(stock_symbols)
