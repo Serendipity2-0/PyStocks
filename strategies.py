381a0a87c8e0f38020a2a2df5ef34d232f1de13e
1Chinmay1 <phchinmay80@gmail.com>
2023-12-27 12:17:08 +0530

    updated strategies to calculate stop loss

--- strategies.py
+++ strategies.py
@@ -2,35 +2,56 @@ import pandas as pd
 import yfinance as yf
 import fetcher
 import TA_indicators
+import StopLoss
 
 def strategy_VolumeBreakout(stockSymbols, period='2d', duration='1d', volume_change_threshold=3):
+    selected_stocks=[]
     for symbol in stockSymbols:
         stock_data = fetcher.get_stock_data(symbol, period, duration)
-        stock_data['Symbol'] = symbol
+        stock_data_monthly = fetcher.get_stock_data(symbol, period="2y", duration="1mo")
+        
         if stock_data is not None and len(stock_data) >= 2:
             volume_changes = stock_data['Volume'].pct_change(periods=1).iloc[-2:]  # Calculate volume changes for last 2 days
             avg_volume_change = volume_changes.mean()
+            
             if avg_volume_change > volume_change_threshold:
-                stock_data.to_csv("significant_volume_changes.csv", mode='a', header=False)  
-                print(f"Stock symbol {symbol} has a volume change more than 3x:\n{stock_data}\n")
+                stoploss = StopLoss.atr_stoploss(stock_data_monthly, atr_window=14)
+                
+                # Calculate the ratio of All-Time High to Last Traded Price
+                all_time_high = stock_data['High'].max()
+                last_traded_price = stock_data['Close'].iloc[-1]
+                ratio_ATH_LTP = all_time_high / last_traded_price
+                
+                selected_stocks.append([symbol, stoploss, ratio_ATH_LTP])
 
+                # Create DataFrame from selected stocks and sort by ratio of ATH and LTP
+                df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol', 'Stoploss', 'Ratio_ATH_LTP'])
+                df_selected_stocks = df_selected_stocks.sort_values(by='Ratio_ATH_LTP', ascending=True)
+    
+                # Save sorted stocks to a CSV file
+                df_selected_stocks.to_csv("significant_volume_changes.csv", mode='w', header=False)
+                print(f"Stock symbol {symbol} has a volume change more than 3x:\n{stock_data}\n")
+    
 def strategy_nr4(stock_symbols, period='5d', duration='1d'):
     selected_stocks = []
     for symbol in stock_symbols:
         stock_data = fetcher.get_stock_data(symbol, period, duration)
+        stock_data_monthly = fetcher.get_stock_data(symbol, period="2y", duration="1mo")
         if stock_data is not None and len(stock_data) >= 5:  # Ensure at least 5 days of data for calculation
             stock_data['HighLowRange'] = stock_data['High'] - stock_data['Low']
             min_range = stock_data['HighLowRange'].tail(4).min()  # Calculate the minimum range in the last 4 days
             today_range = stock_data['High'][-1] - stock_data['Low'][-1]  # Today's range
             if today_range <= min_range:
-                selected_stocks.append({'Symbol': symbol, 'TodayRange': today_range})
-                df_selected_stocks = pd.DataFrame(selected_stocks)
-                df_selected_stocks.to_csv("nr4.csv", mode='a', header=False)
-
+                stoploss=StopLoss.atr_stoploss(stock_data_monthly, atr_window=14)
+                selected_stocks.append([symbol, stoploss,today_range])
+                df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol', 'Stoploss','TOday Range'])
+                df_selected_stocks.to_csv("nr4.csv", mode='w', header=False)
+    
 def strategy_golden_crossover(stock_symbols, period='1y', duration='1d'):
     selected_stocks = []
     for symbol in stock_symbols:
         stock_data = fetcher.get_stock_data(symbol, period, duration)
+        stock_data_monthly = fetcher.get_stock_data(symbol, period="2y", duration="1mo")
         if stock_data is not None and len(stock_data) >= 26:  # Ensure sufficient data for EMA calculation
             stock_data['EMA5'] = TA_indicators.indicator_5EMA(stock_data)
             stock_data['EMA13'] = TA_indicators.indicator_13EMA(stock_data)
@@ -41,10 +62,11 @@ def strategy_golden_crossover(stock_symbols, period='1y', duration='1d'):
                stock_data['EMA13'].iloc[-2] < stock_data['EMA26'].iloc[-2]:
                 if stock_data['EMA5'].iloc[-1] > stock_data['EMA13'].iloc[-1] and \
                 stock_data['EMA13'].iloc[-1] > stock_data['EMA26'].iloc[-1]:
-                    selected_stocks.append({'Symbol': symbol})
-                    df_selected_stocks = pd.DataFrame(selected_stocks)
-                    df_selected_stocks.to_csv('golden_crossover_selected_stocks.csv', index=False)
-
+                    stoploss=StopLoss.atr_stoploss(stock_data_monthly, atr_window=14)
+                    selected_stocks.append([symbol, stoploss])
+                    df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol', 'Stoploss'])
+                    df_selected_stocks.to_csv('golden_crossover_selected_stocks.csv', mode='w', index=False)
+   
 def strategy_above_50EMA(stock_data):
     if stock_data is not None and not stock_data.empty:
         stock_data['EMA_50'] = TA_indicators.indicator_50EMA(stock_data)
@@ -58,7 +80,7 @@ def strategy_momentum(stock_symbols):
     
     for symbol in stock_symbols:
         stock_data = fetcher.get_stock_data(symbol, period="2y", duration="1d")
-        
+        stock_data_monthly = fetcher.get_stock_data(symbol, period="2y", duration="1mo")
         if stock_data is not None and not stock_data.empty:
             # Calculate RSI
             rsi_length_input = 14
@@ -78,15 +100,18 @@ def strategy_momentum(stock_symbols):
             if rsi_values.iloc[-1] > 50 and stock_data['Above_50_EMA'].iloc[-1]:
                 if stock_data['Upper_band'].iloc[-1] < stock_data['Close'].iloc[-1]:
                     if macd.iloc[-1] > signal_line.iloc[-1]:  # Condition 4
-                        selected_stocks.append(symbol)
-                        df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol'])
-                        df_selected_stocks.to_csv("Momentum.csv", mode='a', header=False)  
+                        stoploss=StopLoss.atr_stoploss(stock_data_monthly, atr_window=14)
+                        selected_stocks.append([symbol, stoploss])
+                        df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol', 'Stoploss'])
+                        df_selected_stocks.to_csv("Momentum.csv", mode='w', header=False)
 
+    
 def strategy_mean_reversion(stock_symbols):
     selected_stocks = []
     for symbol in stock_symbols:
         stock_data = fetcher.get_stock_data(symbol, period="2y", duration="1d")
         stock_data_weekly = fetcher.get_stock_data(symbol, period="2y", duration="1wk")
+        stock_data_monthly = fetcher.get_stock_data(symbol, period="2y", duration="1mo")
         if stock_data is not None and not stock_data.empty:
             # Calculate RSI
             rsi_length_input = 14
@@ -103,8 +128,11 @@ def strategy_mean_reversion(stock_symbols):
                 if stock_data_weekly['MA'].iloc[-1] < stock_data_weekly['Close'].iloc[-1]:
                     if stock_data['Lower_band'].iloc[-2] < stock_data['Lower_band'].iloc[-3]:
                         if stock_data['Lower_band'].iloc[-1] > stock_data['Lower_band'].iloc[-2]:
-                            selected_stocks.append(symbol)
-                            df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol'])
-                            df_selected_stocks.to_csv("MeanReversion.csv", mode='a', header=False) 
+                            stoploss=StopLoss.atr_stoploss(stock_data_monthly, atr_window=14)
+                            selected_stocks.append([symbol, stoploss])
+                            df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol', 'Stoploss'])
+                            df_selected_stocks['Stoploss'] = stoploss
+                            df_selected_stocks.to_csv("MeanReversion.csv", mode='w', header=False)
+
 
             
