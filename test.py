2ca37db8e7a9db638a433a41b3c96a34a0d3e74a
1Chinmay1 <phchinmay80@gmail.com>
2023-12-27 12:17:33 +0530

    tested stop loss calculation

--- test.py
+++ test.py
@@ -1,50 +1,38 @@
 import pandas as pd
-import numpy as np
+import fetcher  # Replace with your fetcher module
+import StopLoss  # Replace with your StopLoss module
 
-def ma(source, length, type):
-    if type == "SMA" or type == "Bollinger Bands":
-        return source.rolling(window=length).mean()
-    elif type == "EMA":
-        return source.ewm(span=length, adjust=False).mean()
-    elif type == "SMMA (RMA)":
-        return source.rolling(window=length).apply(lambda x: (x * (2 / (length + 1))).sum() / (2 / (length + 1)), raw=True)
-    elif type == "WMA":
-        weights = np.arange(1, length + 1)
-        wma = source.rolling(window=length).apply(lambda x: np.dot(x, weights) / weights.sum(), raw=True)
-        return wma
-    elif type == "VWMA":
-        volume = pd.Series(data=[1000, 1200, 1500, 800, 950])  # Replace this with actual volume data
-        vwma = (source * volume).rolling(window=length).sum() / volume.rolling(window=length).sum()
-        return vwma
-    else:
-        return None
+def strategy_VolumeBreakout(stockSymbols, period='2d', duration='1d', volume_change_threshold=3):
+    selected_stocks = []
+    
+    for symbol in stockSymbols:
+        stock_data = fetcher.get_stock_data(symbol, period, duration)
+        stock_data_monthly = fetcher.get_stock_data(symbol, period="2y", duration="1mo")
+        
+        if stock_data is not None and len(stock_data) >= 2:
+            volume_changes = stock_data['Volume'].pct_change(periods=1).iloc[-2:]  # Calculate volume changes for last 2 days
+            avg_volume_change = volume_changes.mean()
+            
+            if avg_volume_change > volume_change_threshold:
+                stoploss = StopLoss.atr_stoploss(stock_data_monthly, atr_window=14)
+                
+                # Calculate the ratio of All-Time High to Last Traded Price
+                all_time_high = stock_data['High'].max()
+                last_traded_price = stock_data['Close'].iloc[-1]
+                ratio_ATH_LTP = all_time_high / last_traded_price
+                
+                selected_stocks.append([symbol, stoploss, ratio_ATH_LTP])
 
-def calculate_rsi(data, rsi_length, rsi_source):
-    delta = data[rsi_source].diff()
-    gain = delta.where(delta > 0, 0)
-    loss = -delta.where(delta < 0, 0)
+    # Create DataFrame from selected stocks and sort by ratio of ATH and LTP
+    df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol', 'Stoploss', 'Ratio_ATH_LTP'])
+    df_selected_stocks = df_selected_stocks.sort_values(by='Ratio_ATH_LTP', ascending=True)
     
-    avg_gain = gain.rolling(window=rsi_length).mean()
-    avg_loss = loss.rolling(window=rsi_length).mean()
+    # Save sorted stocks to a CSV file
+    df_selected_stocks.to_csv("significant_volume_changes.csv", mode='a', header=False)
     
-    rs = avg_gain / avg_loss
-    rsi = 100 - (100 / (1 + rs))
-    return rsi
+    return df_selected_stocks
 
 # Example usage
-data = pd.DataFrame({
-    'Close': [100, 110, 90, 95, 105, 115, 120, 125, 130, 120,110,120,200,239,140,100],
-    'Volume': [1000, 1200, 1500, 800, 950, 1100, 900, 850, 1000, 950, 500, 620, 1000,450,2300,4500]  # Replace this with actual volume data
-})
-
-rsi_length_input = 14
-rsi_source_input = 'Close'
-ma_type_input = 'SMA'
-ma_length_input = 14
-bb_mult_input = 2.0
-
-rsi_values = calculate_rsi(data, rsi_length_input, rsi_source_input)
-
-# Printing RSI values and Bollinger Bands (if applicable)
-print("RSI Values:")
-print(rsi_values)
+stock_symbols = ['AAPL', 'GOOGL', 'MSFT']  # Replace with your desired stock symbols
+result = strategy_VolumeBreakout(stock_symbols)
+print(result)
