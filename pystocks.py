8f70d0cabd485a99f7f8d43ef80af6a9733757b1
1Chinmay1 <phchinmay80@gmail.com>
2023-12-27 14:52:42 +0530

    updated Bollinger band Fail strategy

--- pystocks.py
+++ pystocks.py
@@ -4,7 +4,7 @@ import fetcher
 # Fetch stock symbols
 stock_symbols = fetcher.get_stock_codes()
 
-print("Select the Strategy you want to run:\n1. Momentum\n2. Mean Reversion\n3. NR4\n4. Volume Breakout\n5. Golden Crossover")
+print("Select the Strategy you want to run:\n1. Momentum\n2. Mean Reversion\n3. NR4\n4. Volume Breakout\n5. Golden Crossover\n6. Bollinger Band Fail")
 execute_option = int(input("Enter your option: "))
 if execute_option == 1:
     strategies.strategy_momentum(stock_symbols)
@@ -15,7 +15,9 @@ elif execute_option == 3:
 elif execute_option == 4: 
     strategies.strategy_VolumeBreakout(stock_symbols)
 elif execute_option == 5:
-    strategies.strategy_golden_crossover(stock_symbols, period='1y', duration='1d')
+    strategies.strategy_golden_crossover(stock_symbols)
+elif execute_option == 6:
+    strategies.strategy_BollingerBand_Fail(stock_symbols)
 else:
     print("Invalid Option")
     pass
