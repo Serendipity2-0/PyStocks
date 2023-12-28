74ebe3263b5fb5e9ee586bc16ef92780c58f62c6
1Chinmay1 <phchinmay80@gmail.com>
2023-12-28 17:45:31 +0530

    updated mid and long term stock pick modules

--- pystocks.py
+++ pystocks.py
@@ -15,6 +15,33 @@ def shortTerm_pick(stock_symbols):
     shortTerm_stocks.sort(key=lambda x: x[1])
 
     # Store the sorted list in a CSV file
-    df_selected_stocks = pd.DataFrame(shortTerm_stocks, columns=['Symbol', 'ATH_to_LTP_Ratio'])
-    df_selected_stocks.to_csv("combined_selected_stocks.csv", index=False)
+    df_short_selected_stocks = pd.DataFrame(shortTerm_stocks, columns=['Symbol', 'ATH_to_LTP_Ratio'])
+    df_short_selected_stocks.to_csv("shortterm_combined_selected_stocks.csv", index=False)
+
+def midTerm_pick(stock_symbols):
+    momentum_stocks = strategies.strategy_momentum(stock_symbols)
+    volume_breakout = strategies.strategy_VolumeBreakout(stock_symbols)
+    # Combine selected stocks from all strategies
+    midTerm_stocks = momentum_stocks + volume_breakout
+
+    # Sort the combined list based on ATH to LTP ratio in ascending order
+    midTerm_stocks.sort(key=lambda x: x[1])
+
+    df_mid_selected_stocks = pd.DataFrame(midTerm_stocks, columns=['Symbol', 'ATH_to_LTP_Ratio'])
+    df_mid_selected_stocks.to_csv("midterm_combined_selected_stocks.csv", index=False)
+
+def longTerm_pick(stock_symbols):
+    golden_crossover_stocks = strategies.strategy_golden_crossover(stock_symbols)
+    # Combine selected stocks from all strategies
+    longTerm_stocks = golden_crossover_stocks
+
+    # Sort the combined list based on ATH to LTP ratio in ascending order
+    longTerm_stocks.sort(key=lambda x: x[1])
+
+    df_long_selected_stocks = pd.DataFrame(longTerm_stocks, columns=['Symbol', 'Stoploss'])
+    df_long_selected_stocks.to_csv("longterm_combined_selected_stocks.csv", index=False)
+
+
 shortTerm_pick(stock_symbols)
+midTerm_pick(stock_symbols)
+longTerm_pick(stock_symbols)
