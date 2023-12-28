18b2941b305869d83951cbe1eeed9ca7cb7abc19
1Chinmay1 <phchinmay80@gmail.com>
2023-12-28 17:16:02 +0530

    updated return statements and fixed errors

--- strategies.py
+++ strategies.py
@@ -4,34 +4,32 @@ import fetcher
 import TA_indicators
 import StopLoss
 
+selected_stocks=[]
 def strategy_VolumeBreakout(stockSymbols, period='2d', duration='1d', volume_change_threshold=3):
     selected_stocks=[]
     for symbol in stockSymbols:
         stock_data = fetcher.get_stock_data(symbol, period, duration)
-        stock_data_monthly = fetcher.get_stock_data(symbol, period="2y", duration="1mo")
-        
         if stock_data is not None and len(stock_data) >= 2:
             volume_changes = stock_data['Volume'].pct_change(periods=1).iloc[-2:]  # Calculate volume changes for last 2 days
             avg_volume_change = volume_changes.mean()
             
             if avg_volume_change > volume_change_threshold:
-                stoploss = StopLoss.atr_stoploss(stock_data_monthly, atr_window=14)
                 
                 # Calculate the ratio of All-Time High to Last Traded Price
                 all_time_high = stock_data['High'].max()
                 last_traded_price = stock_data['Close'].iloc[-1]
                 ratio_ATH_LTP = all_time_high / last_traded_price
                 
-                selected_stocks.append([symbol, stoploss, ratio_ATH_LTP])
+                selected_stocks.append([symbol, ratio_ATH_LTP])
 
                 # Create DataFrame from selected stocks and sort by ratio of ATH and LTP
-                df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol', 'Stoploss', 'Ratio_ATH_LTP'])
-                df_selected_stocks = df_selected_stocks.sort_values(by='Ratio_ATH_LTP', ascending=True)
+                #df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol', 'Stoploss', 'Ratio_ATH_LTP'])
+                #df_selected_stocks = df_selected_stocks.sort_values(by='Ratio_ATH_LTP', ascending=True)
     
                 # Save sorted stocks to a CSV file
-                df_selected_stocks.to_csv("significant_volume_changes.csv", mode='w', header=False)
-                print(f"Stock symbol {symbol} has a volume change more than 3x:\n{stock_data}\n")
-    
+                #df_selected_stocks.to_csv("significant_volume_changes.csv", mode='w', header=False)
+                #print(f"Stock symbol {symbol} has a volume change more than 3x:\n{stock_data}\n")
+    return selected_stocks
 def strategy_nr4(stock_symbols, period='5d', duration='1d'):
     selected_stocks = []
     for symbol in stock_symbols:
@@ -76,11 +74,13 @@ def strategy_above_50EMA(stock_data):
         return None
     
 def strategy_momentum(stock_symbols):
+    global selected_stocks
     selected_stocks = []
-    
     for symbol in stock_symbols:
-        stock_data = fetcher.get_stock_data(symbol, period="2y", duration="1d")
-        stock_data_monthly = fetcher.get_stock_data(symbol, period="2y", duration="1mo")
+        try:
+            stock_data = fetcher.get_stock_data(symbol, period="2y", duration="1d")
+        except:
+            pass
         if stock_data is not None and not stock_data.empty:
             # Calculate RSI
             rsi_length_input = 14
@@ -100,18 +100,23 @@ def strategy_momentum(stock_symbols):
             if rsi_values.iloc[-1] > 50 and stock_data['Above_50_EMA'].iloc[-1]:
                 if stock_data['Upper_band'].iloc[-1] < stock_data['Close'].iloc[-1]:
                     if macd.iloc[-1] > signal_line.iloc[-1]:  # Condition 4
-                        stoploss=StopLoss.atr_stoploss(stock_data_monthly, atr_window=14)
-                        selected_stocks.append([symbol, stoploss])
-                        df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol', 'Stoploss'])
-                        df_selected_stocks.to_csv("Momentum.csv", mode='w', header=False)
-
+                        all_time_high = stock_data['High'].max()
+                        last_traded_price = stock_data['Close'].iloc[-1]
+                        ratio_ATH_LTP = all_time_high / last_traded_price
+                        selected_stocks.append([symbol, ratio_ATH_LTP])
+                        #df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol', 'Stoploss'])
+                        #df_selected_stocks.to_csv("Momentum.csv", mode='w', header=False)         
+    return selected_stocks
     
 def strategy_mean_reversion(stock_symbols):
+    global selected_stocks
     selected_stocks = []
     for symbol in stock_symbols:
-        stock_data = fetcher.get_stock_data(symbol, period="2y", duration="1d")#for 15 min duration = 15m period = 50d, for hourly duration = 1h period = 1y
-        stock_data_weekly = fetcher.get_stock_data(symbol, period="2y", duration="1wk")
-        stock_data_monthly = fetcher.get_stock_data(symbol, period="2y", duration="1mo")
+        try:
+            stock_data = fetcher.get_stock_data(symbol, period="2y", duration="1d")#for 15 min duration = 15m period = 50d, for hourly duration = 1h period = 1y
+            stock_data_weekly = fetcher.get_stock_data(symbol, period="2y", duration="1wk")
+        except:
+            pass
         if stock_data is not None and not stock_data.empty:
             # Calculate RSI
             rsi_length_input = 14
@@ -128,19 +133,23 @@ def strategy_mean_reversion(stock_symbols):
                 if stock_data_weekly['MA'].iloc[-1] < stock_data_weekly['Close'].iloc[-1]:
                     if stock_data['Lower_band'].iloc[-2] < stock_data['Lower_band'].iloc[-3]:
                         if stock_data['Lower_band'].iloc[-1] > stock_data['Lower_band'].iloc[-2]:
-                            stoploss=StopLoss.atr_stoploss(stock_data_monthly, atr_window=14)
-                            selected_stocks.append([symbol, stoploss])
-                            df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol', 'Stoploss'])
-                            df_selected_stocks['Stoploss'] = stoploss
-                            df_selected_stocks.to_csv("MeanReversion.csv", mode='w', header=False)
-
+                            all_time_high = stock_data['High'].max()
+                            last_traded_price = stock_data['Close'].iloc[-1]
+                            ratio_ATH_LTP = all_time_high / last_traded_price
+                            selected_stocks.append([symbol, ratio_ATH_LTP])
+                            #df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol'])
+                            #df_selected_stocks.to_csv("MeanReversion.csv", mode='w', header=False)
+    return selected_stocks
 
 def strategy_BollingerBand_Fail(stock_symbols):
     selected_stocks = []
     for symbol in stock_symbols:
-        stock_data_daily = fetcher.get_stock_data(symbol, period="2y", duration="1d")
-        stock_data_weekly = fetcher.get_stock_data(symbol, period="2y", duration="1wk")
-        stock_data_monthly = fetcher.get_stock_data(symbol, period="2y", duration="1mo")
+        try:
+            stock_data_daily = fetcher.get_stock_data(symbol, period="2y", duration="1d")
+            stock_data_weekly = fetcher.get_stock_data(symbol, period="2y", duration="1wk")
+            stock_data_monthly = fetcher.get_stock_data(symbol, period="2y", duration="1mo")
+        except:
+            pass
         # Calculate Bollinger Bands
         bb_window = 20
         stock_data_daily = TA_indicators.indicator_bollinger_bands(stock_data_daily, bb_window)
@@ -155,6 +164,7 @@ def strategy_BollingerBand_Fail(stock_symbols):
                         df_selected_stocks.to_csv("BollingerBand_Fail.csv", mode='w', header=False)
             
 def strategy_EMA_BB_Confluence(stock_symbols):
+    global selected_stocks
     selected_stocks =[]
     for symbol in stock_symbols:
         try:
@@ -168,9 +178,13 @@ def strategy_EMA_BB_Confluence(stock_symbols):
                         if stock_data['Lower_band'].iloc[-1] > stock_data['Lower_band'].iloc[-2]:
                             bollinger_close_to_ema = abs(stock_data['Lower_band'].iloc[-1] - stock_data['EMA_50'].iloc[-1]) < 0.05 * stock_data['Close'].iloc[-1]
                             if bollinger_close_to_ema:
-                                selected_stocks.append([symbol])
-                                df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol'])
-                                df_selected_stocks.to_csv("selected stocks.csv", mode='w', header=False)
+                                all_time_high = stock_data['High'].max()
+                                last_traded_price = stock_data['Close'].iloc[-1]
+                                ratio_ATH_LTP = all_time_high / last_traded_price
+                                selected_stocks.append([symbol, ratio_ATH_LTP])
+                                #df_selected_stocks = pd.DataFrame(selected_stocks, columns=['Symbol'])
+                                #df_selected_stocks.to_csv("selected stocks.csv", mode='w', header=False)
+                                break
         except:
             pass
-    print("completed Scanning")
+    return selected_stocks
