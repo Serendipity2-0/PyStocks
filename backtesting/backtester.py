f37a3fe68c15a65b503b7c0976aa5a16c32c410f
1Chinmay1 <phchinmay80@gmail.com>
2024-01-11 10:00:56 +0530

    updated for strategy module

--- backtesting/backtester.py
+++ backtesting/backtester.py
@@ -1,45 +1,5 @@
 import pandas as pd
-import Backtesting_Utils
-
-# Function to implement the backtesting strategy
-def strategy_VolumeBreakout(stock_data, volume_change_threshold=3):
-    if stock_data is not None and len(stock_data) >= 2:
-        volume_changes = stock_data['Volume'].pct_change(periods=1).iloc[-2:]  # Calculate volume changes for last 2 days
-        avg_volume_change = volume_changes.mean()
-        
-        if avg_volume_change > volume_change_threshold:
-            # Calculate stop loss and target based on strategy rules
-            entry_price = stock_data['Close'].iloc[-1]  # Entry price
-            stop_loss = 0.10 * entry_price
-            stop = entry_price - stop_loss
-            target = entry_price + (stop * 3)  # 3x the stop loss
-            
-            return {'Signal': 'Buy', 'Entry_price': entry_price, 'Stop_loss': stop_loss, 'Target': target}
-        else:
-            return {'Signal': 'No Signal'}
-    else:
-        return {'Signal': 'Insufficient Data'}
-
-def strategy_EMA_Crossover(stock_data):
-    if stock_data is not None and len(stock_data) >= 53:  # Ensure sufficient data for EMA calculation
-        stock_data = stock_data.copy()  # Create a copy to avoid modifying the original DataFrame
-        
-        stock_data['EMA5'] = Backtesting_Utils.indicator_5EMA(stock_data)
-        stock_data['EMA13'] = Backtesting_Utils.indicator_13EMA(stock_data)
-        stock_data['EMA26'] = Backtesting_Utils.indicator_26EMA(stock_data)
-
-        # Check for Golden Crossover
-        if (stock_data['EMA5'].iloc[-2] < stock_data['EMA13'].iloc[-2] and
-            stock_data['EMA13'].iloc[-2] < stock_data['EMA26'].iloc[-2]):
-            # Check if the crossover occurred in the last day
-            if (stock_data['EMA5'].iloc[-1] > stock_data['EMA13'].iloc[-1] and
-                stock_data['EMA13'].iloc[-1] > stock_data['EMA26'].iloc[-1]):
-                entry_price = stock_data['Close'].iloc[-1]  # Entry price
-                stop_loss = 0.05 * entry_price
-                stop = entry_price - stop_loss
-                target = entry_price + (stop * 3)  # 3x the stop loss
-                return {'Signal': 'Buy', 'Entry_price': entry_price, 'Stop_loss': stop_loss, 'Target': target}
-    return {'Signal': 'No Signal'}
+import strategy
 
 # Load historical stock data
 data = pd.read_csv('RELIANCE.csv')  # Replace 'RELIANCE.csv' with the filename of Reliance historical data
@@ -54,7 +14,7 @@ for i in range(len(data)):
     stock_data = data.iloc[:i + 1].copy()  # Iterate through historical data from the beginning up to the current index
     
     # Apply strategy function to get signals
-    strategy_result = strategy_EMA_Crossover(stock_data)
+    strategy_result = strategy.strategy_EMA_Crossover(stock_data)
     
     if strategy_result['Signal'] == 'Buy':
         entry = strategy_result['Entry_price']
