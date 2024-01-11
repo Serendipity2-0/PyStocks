6924b4b1df90d1f1d053be97155e2541dad770f9
1Chinmay1 <phchinmay80@gmail.com>
2024-01-12 00:39:38 +0530

    updated trailing Stop loss Calculation

--- backtesting/tes.py
+++ backtesting/tes.py
@@ -2,7 +2,7 @@ import pandas as pd
 import strategy
 
 # Load historical stock data
-data = pd.read_csv('RELIANCE.csv')  # Replace 'RELIANCE.csv' with the filename of Reliance historical data
+data = pd.read_csv('C:\\Users\\phchi\\Desktop\\python - Serebdipity\\PyStocks\\INFY.csv')  # Replace 'RELIANCE.csv' with the filename of Reliance historical data
 
 # Backtesting and calculating Net PnL and Hit Ratio
 capital = 1000000  # Initial capital
@@ -14,7 +14,7 @@ for i in range(len(data)):
     stock_data = data.iloc[:i + 1].copy()  # Iterate through historical data from the beginning up to the current index
     
     # Apply strategy function to get signals
-    strategy_result = strategy.strategy_EMA_Crossover(stock_data)
+    strategy_result = strategy.strategy_momentum(stock_data)
     
     if strategy_result['Signal'] == 'Buy':
         entry = strategy_result['Entry_price']
@@ -24,6 +24,7 @@ for i in range(len(data)):
         current_index = i
         trailing_stop_loss = stop_loss
         stop = entry - trailing_stop_loss
+        previous_day_close = entry
         
         # Loop through stock data until stop loss or target is triggered
         while current_index < len(data):
@@ -31,9 +32,9 @@ for i in range(len(data)):
             current_price_high = data['High'].iloc[current_index] 
             current_price_low = data['Low'].iloc[current_index]
             
-            if current_price_close >= entry + (stop * 0.5):
+            if current_price_close >= previous_day_close + (stop * 0.5):
                 trailing_stop_loss = trailing_stop_loss + (trailing_stop_loss * 0.5)  # Update trailing stop loss
-                stop = entry - trailing_stop_loss
+                previous_day_close = current_price_close
             
             if current_price_close <= trailing_stop_loss or current_price_high <= trailing_stop_loss or current_price_low <= trailing_stop_loss:
                 if trailing_stop_loss > entry:
