539301fbff04d0bd748c442529a32538346f7a41
1Chinmay1 <137078297+1Chinmay1@users.noreply.github.com>
2024-01-14 23:38:08 +0530

    Update strategy.py

--- backtesting/strategy.py
+++ backtesting/strategy.py
@@ -63,51 +63,6 @@ def strategy_momentum(stock_data):
                         target = entry_price + (0.10 * entry_price * 3) 
                         return {'Signal': 'Buy', 'Entry_price': entry_price, 'Stop_loss': stop_loss, 'Target': target}
     return {'Signal': 'No Signal'} 
-
-def strategy_vwap(stock_data):
-    if stock_data is not None and len(stock_data) >= 103:
-            stock_data['EMA5'] = Backtesting_Utils.indicator_5EMA(stock_data)
-            stock_data['EMA13'] = Backtesting_Utils.indicator_13EMA(stock_data)
-            stock_data['EMA26'] = Backtesting_Utils.indicator_26EMA(stock_data)
-            stock_data['EMA50'] = Backtesting_Utils.indicator_50EMA(stock_data)
-            stock_data['VWAP'] = Backtesting_Utils.indicator_vwap(stock_data)
-
-            if (
-                stock_data['Close'].iloc[-2] < stock_data['EMA5'].iloc[-2] and
-                stock_data['Close'].iloc[-2] < stock_data['EMA13'].iloc[-2] and
-                stock_data['Close'].iloc[-2] < stock_data['EMA26'].iloc[-2] and
-                stock_data['Close'].iloc[-2] < stock_data['EMA50'].iloc[-2] and
-                stock_data['Close'].iloc[-2] < stock_data['VWAP'].iloc[-2]
-            ):
-                if (
-                    stock_data['Close'].iloc[-1] > stock_data['EMA5'].iloc[-1] and
-                    stock_data['Close'].iloc[-1] > stock_data['EMA13'].iloc[-1] and
-                    stock_data['Close'].iloc[-1] > stock_data['EMA26'].iloc[-1] and
-                    stock_data['Close'].iloc[-1] > stock_data['EMA50'].iloc[-1] and
-                    stock_data['Close'].iloc[-1] > stock_data['VWAP'].iloc[-1]
-                ):
-                    entry_price = stock_data['Close'].iloc[-1]  # Entry price
-                    stop_loss = entry_price-(0.10 * entry_price)
-                    target = entry_price + (0.10 * entry_price * 3) 
-                    return {'Signal': 'Buy', 'Entry_price': entry_price, 'Stop_loss': stop_loss, 'Target': target}
-    return {'Signal': 'No Signal'} 
-
-def strategy_ema_bb_confluence(stock_data):
-    if stock_data is not None and not stock_data.empty:
-        bb_window = 20
-        stock_data = Backtesting_Utils.indicator_bollinger_bands(stock_data, bb_window)
-        stock_data['EMA_50'] = Backtesting_Utils.indicator_50EMA(stock_data)
-        if stock_data['EMA_50'].iloc[-1] <= stock_data['Lower_band'].iloc[-1]:
-            if stock_data['Close'].iloc[-1] < stock_data['MA'].iloc[-1]:
-                if stock_data['Lower_band'].iloc[-2] < stock_data['Lower_band'].iloc[-3]:
-                    if stock_data['Lower_band'].iloc[-1] > stock_data['Lower_band'].iloc[-2]:
-                        bollinger_close_to_ema = abs(stock_data['Lower_band'].iloc[-1] - stock_data['EMA_50'].iloc[-1]) < 0.05 * stock_data['Close'].iloc[-1]
-                        if bollinger_close_to_ema and stock_data['Volume'].iloc[-1] > stock_data['Volume'].rolling(window=20).mean().iloc[-1]:
-                            entry_price = stock_data['Close'].iloc[-1]  # Entry price
-                            stop_loss = entry_price-(0.10 * entry_price)
-                            target = entry_price + (0.10 * entry_price * 3) 
-                            return {'Signal': 'Buy', 'Entry_price': entry_price, 'Stop_loss': stop_loss, 'Target': target}
-    return {'Signal': 'No Signal'} 
     
 def strategy_5_50_EMA_Crossover(stock_data):
     if stock_data is not None and len(stock_data) >= 102:  # Ensure sufficient data for EMA calculation
